bd7f91bfa61cdc1eff9304739c1fd896cd2d147b
eric <eric.y.corsican@gmail.com>
2019-10-03 11:39:44 +0800

    fix build

--- tun2socks.go
+++ tun2socks.go
@@ -20,8 +20,8 @@ func InputPacket(data []byte) {
 func StartSocks(packetFlow PacketFlow, proxyHost string, proxyPort int) {
 	if packetFlow != nil {
 		lwipStack = core.NewLWIPStack()
-		core.RegisterTCPConnHandler(socks.NewTCPHandler(proxyHost, uint16(proxyPort), nil))
-		core.RegisterUDPConnHandler(socks.NewUDPHandler(proxyHost, uint16(proxyPort), 2*time.Minute, nil, nil))
+		core.RegisterTCPConnHandler(socks.NewTCPHandler(proxyHost, uint16(proxyPort)))
+		core.RegisterUDPConnHandler(socks.NewUDPHandler(proxyHost, uint16(proxyPort), 2*time.Minute))
 		core.RegisterOutputFn(func(data []byte) (int, error) {
 			packetFlow.WritePacket(data)
 			return len(data), nil
